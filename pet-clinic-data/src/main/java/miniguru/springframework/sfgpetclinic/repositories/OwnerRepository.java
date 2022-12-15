49daae54d345a6e4aabc028db971a7976c658950
hilal.kasem24 <hilal.kasem24@gmail.com>
2022-12-15 02:18:31 +0100

    Created Spring Data JPA Service for Owners. Closes #35.

--- pet-clinic-data/src/main/java/miniguru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/miniguru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import miniguru.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
