708a5099401ed0cd9bfd397f1a8a139902b5f02f
hilal.kasem24 <hilal.kasem24@gmail.com>
2022-12-15 03:35:34 +0100

    Created Visit Map Service. Closes #41.

---
+++ pet-clinic-data/src/main/java/miniguru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package miniguru.springframework.sfgpetclinic.services;
+
+import miniguru.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+}
