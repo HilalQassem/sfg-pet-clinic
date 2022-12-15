d56502be13dcdc9303e5947d48d9fd62784d058f
HilalQassem <116579035+HilalQassem@users.noreply.github.com>
2022-12-15 17:36:08 +0100

    Added Project Lombok and Refactored. Closes #46.

--- pet-clinic-data/src/main/java/miniguru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/miniguru/springframework/sfgpetclinic/model/PetType.java
@@ -1,23 +1,19 @@
 package miniguru.springframework.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-/**
- * Created by jt on 7/13/18.
- */
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "types")
 public class PetType extends BaseEntity {
     @Column(name = "name")
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- pet-clinic-data/src/main/java/miniguru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/miniguru/springframework/sfgpetclinic/model/Vet.java
@@ -1,13 +1,17 @@
 package miniguru.springframework.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * Created by jt on 7/13/18.
- */
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "vets")
 public class Vet extends Person {
     @ManyToMany(fetch = FetchType.EAGER)
@@ -16,11 +20,4 @@ public class Vet extends Person {
             inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
-    public Set<Specialty> getSpecialties() {
-        return specialties;
-    }
-
-    public void setSpecialties(Set<Specialty> specialties) {
-        this.specialties = specialties;
-    }
 }
